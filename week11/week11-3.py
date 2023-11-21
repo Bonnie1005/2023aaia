ed44c0b308ca0bbf319157bd39acfb1e35f3b361
Bonnie1005 <12750804@me.mcu.edu.tw>
2023-11-21 11:06:15 +0800

    week11

---
+++ week11/week11-3.py
@@ -0,0 +1,9 @@
+# week11-3a.py SOIT106_BASE_003
+a = list(map(int, input().split() ))
+
+print( sum(a) - a[0] )
+
+#week11-3b.py SOIT106_BASE_006
+a = list(map(int, input().split() ))
+
+print( max(a) )
